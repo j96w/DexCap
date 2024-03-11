6be24dd82b66ef418cce500eb30e13c7a593f2bb
j96w <j96w@qq.com>
2024-03-11 00:15:38 -0700

    update configs

--- STEP2_build_dataset/demo_create_hdf5.py
+++ STEP2_build_dataset/demo_create_hdf5.py
@@ -7,9 +7,9 @@ sub_dirs = [os.path.join(dataset_base_dir, d) for d in os.listdir(dataset_base_d
 dataset_folders = sorted(sub_dirs, key=lambda d: extract_dataset_folder_last_two_digits(os.path.basename(d)))
 
 action_gap = 5
-num_points_to_sample = 100000
+num_points_to_sample = 10000
 
-# output_hdf5_file = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/hand_wiping_1-14_{}actiongap_{}points_2.hdf5'.format(action_gap, num_points_to_sample)
+# output_hdf5_file = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/hand_wiping_1-14_{}actiongap_{}points.hdf5'.format(action_gap, num_points_to_sample)
 output_hdf5_file = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/hand_packaging_1-20_{}actiongap_{}points.hdf5'.format(action_gap, num_points_to_sample)
 
 # process_hdf5(output_hdf5_file, dataset_folders, action_gap, num_points_to_sample)
