a0a06bb20b994236af7c1b6e9c03d42ab05695a1
j96w <j96w@qq.com>
2024-03-11 23:16:57 -0700

    cleanup

--- STEP2_build_dataset/demo_create_hdf5.py
+++ STEP2_build_dataset/demo_create_hdf5.py
@@ -1,7 +1,7 @@
 from dataset_utils import *
 
 # dataset_base_dir = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/save_wipe_1-14'
-dataset_base_dir = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/save_data_packaging_example'
+dataset_base_dir = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/save_packaging_wild_1-20'
 
 sub_dirs = [os.path.join(dataset_base_dir, d) for d in os.listdir(dataset_base_dir) if os.path.isdir(os.path.join(dataset_base_dir, d))]
 dataset_folders = sorted(sub_dirs, key=lambda d: extract_dataset_folder_last_two_digits(os.path.basename(d)))
@@ -10,7 +10,7 @@ action_gap = 5
 num_points_to_sample = 10000
 
 # output_hdf5_file = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/hand_wiping_1-14_{}actiongap_{}points.hdf5'.format(action_gap, num_points_to_sample)
-output_hdf5_file = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/hand_packaging_1-20_{}actiongap_{}points.hdf5'.format(action_gap, num_points_to_sample)
+output_hdf5_file = '/media/jeremy/cde0dfff-70f1-4c1c-82aa-e0d469c14c62/franka_leap_control_datasets/hand_packaging_wild_1-20_{}actiongap_{}points.hdf5'.format(action_gap, num_points_to_sample)
 
 # process_hdf5(output_hdf5_file, dataset_folders, action_gap, num_points_to_sample)
 process_hdf5(output_hdf5_file, dataset_folders, action_gap, num_points_to_sample, in_wild_data=True)
